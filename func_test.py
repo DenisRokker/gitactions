9696ed87ed5dac85eca8d439522d22e17aee4c06
rokkerdenis <denis.rokker@gmail.com>
2020-08-16 16:38:47 +0300

    add database test

--- func_test.py
+++ func_test.py
@@ -1,4 +1,5 @@
 from .func_speed import time, speed, distance, name
+import psycopg2
 
 
 def test_speed_calculation():
@@ -15,3 +16,32 @@ def test_distance_calculation():
 
 def test_name():
     assert type(name()) == str
+
+
+def test_create_table():
+    connect = psycopg2.connect(dbname="postgres",
+                               user="postgres",
+                               password="postgres",
+                               host='localhost')
+    cursor = connect.cursor()
+
+    cursor.execute('create table People('
+                   'ID INT NOT NULL,'
+                   'NAME TEXT,'
+                   'AGE INT NOT NULL,'
+                   'PRIMARY KEY (id));')
+
+    cursor.execute("insert into People (id, name, age) values(1, 'Denis', 21);")
+    cursor.execute("insert into People (id, name, age) values(2, 'not Denis', 16);")
+    cursor.execute("insert into People (id, name, age) values(3, 'Denis', 66);")
+    cursor.execute("insert into People (id, name, age) values(4, 'Denis', 38);")
+    cursor.execute("insert into People (id, name, age) values(5, 'not Denis', 2);")
+
+    cursor.execute("select * from People where name = 'Denis';")
+
+    assert cursor.fetchall()
+    cursor.execute('drop table People;')
+
+    connect.commit()
+    cursor.close()
+    connect.close()
