b20942a857bed5fc7719977d5f46654195f18396
rokkerdenis <denis.rokker@gmail.com>
2020-08-24 17:30:23 +0300

    up

--- hello_world_test.py
+++ hello_world_test.py
@@ -1,7 +1,13 @@
+import os
+
 import requests
 
 
 def test_open():
-    r = requests.get("http://dockerhost:8080/")
+    host = os.environ.get("TESTS_WEB_HOST")
+    if host is None:
+        host = "localhost"
+
+    r = requests.get(f"http://{host}:8080/")
 
     assert r.status_code == 200, r.text
